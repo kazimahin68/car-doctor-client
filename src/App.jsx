55b081a77211d11984b6a3d6d67d0a854e176463
kazimahin68 <kazimahin68@gmail.com>
2023-05-15 17:11:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h2>Nothing in it</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../shared/Footer";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is Home page</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
