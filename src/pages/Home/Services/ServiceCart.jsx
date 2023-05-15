8046bb23f97d9308c9dd5b0117333e91a2879bc0
kazimahin68 <kazimahin68@gmail.com>
2023-05-16 02:18:15 +0600

    services section added

---
+++ src/pages/Home/Services/ServiceCart.jsx
@@ -0,0 +1,30 @@
+import { PropTypes } from "prop-types";
+import { FaAngleRight } from "react-icons/fa";
+
+const ServiceCart = ({ service }) => {
+
+    const { title, img, price } = service;
+    console.log(service)
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl p-5">
+            <figure><img src={img} alt="" className="rounded-xl" /></figure>
+            <div className="card-body">
+                <h2 className="card-title font-bold text-2xl">{title}</h2>
+                <div className="flex justify-between items-center">
+                    <p className="font-bold text-orange-600">Price: $ {price}</p>
+                    <button className="btn text-orange-600 btn-outline border-none"><FaAngleRight></FaAngleRight></button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ServiceCart.propTypes = {
+    service: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        img: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default ServiceCart;
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
+import ServiceCart from "./ServiceCart";
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
     return (
         <div>
             <div className="w-1/2 mx-auto text-center mt-12">
                 <h4 className="text-orange-600 font-bold mb-4">Service Area</h4>
                 <h1 className="font-extrabold text-5xl mb-4">Our Service Area</h1>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomized words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mt-12">
+                {
+                    services.map(service => <ServiceCart key={service._id} service={service}></ServiceCart>)
+                }
             </div>
         </div>
     );
